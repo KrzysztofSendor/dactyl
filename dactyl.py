760506e88d22d86be818017fb6075abe7af2a068
KrzysztofSendor <sendor.krzysztof@gmail.com>
2016-08-24 12:48:57 +0200

    Add url_validator function and respond aciton to test url

--- dactyl.py
+++ dactyl.py
@@ -2,3 +2,19 @@ from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 import re
 import urllib
+
+
+def url_validator(url):
+    try:
+        code = urllib.urlopen(url).getcode()
+        if code == 200:
+            return True
+    except:
+        return False
+
+
+def test_url(message, url):
+    if url_validator(url[1:len(url)-1]):
+        message.reply('VALID URL')
+    else:
+        message.reply('NOT VALID URL')
